70f2e56e03870f325d69763da8ed2a8d2c640b87
tikrack <tikrackcode@gmail.com>
2025-08-22 22:46:12 +0330

    [Initial]

--- demo/main.js
+++ demo/main.js
@@ -1,1 +1,20 @@
-const { TelegramClient } = require("../index");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { Telenov } = require("../index");
+
+const bot = new Telenov("7690980872:AAHuLwq6QvBD3wYv4y_PzRWUmUOH-NP-Y5Q");
+//
+// bot.onMessage((ctx) => ctx.reply("پیام دریافت شد ✅"));
+// bot.onCommand("start", (ctx) => ctx.reply("سلام ✨ خوش اومدی!"));
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/webhook", (req, res) => {
+    res.sendStatus(200);
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+    bot.setWebhook("https://YOUR_DOMAIN/webhook"); // ثبت وبهوک روی تلگرام
+});
--- src/client.js
+++ src/client.js
@@ -1,5 +1,9 @@
 class Telenov {
+    constructor(token) {
+        if (!token) throw new Error("Telenov => Please set a correct token.");
 
+        this.token = token;
+    }
 }
 
 module.exports = Telenov;
